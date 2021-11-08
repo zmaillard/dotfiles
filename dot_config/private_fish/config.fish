821abef1a90b910fb02cf224ce5a4066315b54e5
Zachary Maillard <zmaillard@healthwise.org>
2021-11-08 13:33:32 -0700

    More Fish Config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_vi_key_bindings
+    starship init fish | source
 end
 
-starship init fish | source
+alias vi="nvim"
+alias vim="nvim"
+alias python="python3"
 
+
+if test -d ~/.local/bin
+  set -gx PATH $HOME/.local/bin $PATH
+end
+
+if test -d ~/.asdf
+  source ~/.asdf/asdf.fish
+end
