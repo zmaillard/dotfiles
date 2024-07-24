131db885de2285feeae62eff82e9fb89c56a5f1f
Zachary Maillard <zmaillard@healthwise.org>
2024-07-24 14:19:39 -0600

    1Password and Nvim Updates

--- home/dot_config/nvim/lua/zach/remap.lua
+++ home/dot_config/nvim/lua/zach/remap.lua
@@ -19,3 +19,10 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
