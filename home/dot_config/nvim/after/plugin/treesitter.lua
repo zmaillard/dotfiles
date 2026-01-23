cc694e4a82df8c925c2b01dabc38935d86a50e0a
Zachary Maillard <zmaillard@protonmail.com>
2026-01-23 07:18:38 -0700

    Plugin Cleanup / Treesitter

--- home/dot_config/nvim/after/plugin/treesitter.lua
+++ home/dot_config/nvim/after/plugin/treesitter.lua
@@ -2,3 +2,19 @@ local treesitter = require("nvim-treesitter")
 treesitter.setup()
 treesitter.install ({  'javascript', 'typescript', 'c', 'lua', 'vim', 'vimdoc', 'query', 'clojure' })
       
+
+vim.api.nvim_create_autocmd('FileType', {
+pattern = {   'javascript', 'typescript', 'c', 'lua', 'vim', 'vimdoc', 'query', 'clojure' },
+callback = function()
+    -- syntax highlighting, provided by Neovim
+    vim.treesitter.start()
+    
+
+    -- Uncomment below to enable folds
+    --vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    --vim.wo.foldmethod = 'expr'
+
+    -- indentation, provided by nvim-treesitter
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+end,
+})
--- home/dot_config/nvim/lua/zach/remap.lua
+++ home/dot_config/nvim/lua/zach/remap.lua
@@ -27,3 +27,6 @@ vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
