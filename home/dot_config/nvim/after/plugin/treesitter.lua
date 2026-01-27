8abfd1a75f65f1315785de3fa97a52292a14c673
Zachary Maillard <zmaillard@protonmail.com>
2026-01-27 07:32:12 -0700

    Bufferline

--- home/dot_config/nvim/after/plugin/treesitter.lua
+++ home/dot_config/nvim/after/plugin/treesitter.lua
@@ -1,10 +1,10 @@
 local treesitter = require("nvim-treesitter")
 treesitter.setup()
-treesitter.install ({  'javascript', 'typescript', 'c', 'lua', 'vim', 'vimdoc', 'query', 'clojure' })
+treesitter.install ({  'javascript', 'typescript', 'c', 'lua', 'python', 'vim', 'vimdoc', 'query', 'clojure' })
       
 
 vim.api.nvim_create_autocmd('FileType', {
-pattern = {   'javascript', 'typescript', 'c', 'lua', 'vim', 'vimdoc', 'query', 'clojure' },
+pattern = {   'javascript', 'typescript', 'c', 'lua', 'python', 'vim', 'vimdoc', 'query', 'clojure' },
 callback = function()
     -- syntax highlighting, provided by Neovim
     vim.treesitter.start()
